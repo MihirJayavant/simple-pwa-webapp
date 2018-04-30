29b450ca963e8b36a33f9514385bedd38db009b5
mihir jayavant <mihir.jayavant2@gmail.com>
2018-04-30 17:09:39 +0530

    Webpack 2 setup done for todo

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+    filename: 'main.css'
+ });
+
+
+module.exports = { 
+
+    entry: './src/js/index.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        //publicPath: '/dist'
+      },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: extractPlugin.extract({
+                        use: ['css-loader']
+                    })
+            },
+            {
+                test: /\.js$/,
+                use: {loader: 'babel-loader', options: { presets: ['es2015'] }}
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: {loader: 'file-loader', options: {
+                    outputPath: 'assets/',
+                    publicPath: 'assets/'
+                }}
+            }
+        ]
+    },
+    plugins: [
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+
+    devServer: {
+        port: 1234,
+        //contentBase: path.join(__dirname, "src")
+    }
+
+}
